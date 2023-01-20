0b4733a15d75f194f2c3aa4c767a27b35cd1f959
Walaff Fernandes <walaffonofre@icloud.com>
2023-01-20 10:33:35 -0300

    chore(provider): :sparkles: bind dependency inversion

---
+++ providers/TrpcProvider.ts
@@ -0,0 +1,21 @@
+import { ApplicationContract } from '@ioc:Adonis/Core/Application'
+
+export default class TrpcProvider {
+  constructor(protected app: ApplicationContract) {}
+
+  public register() {
+    const config = this.app.container.resolveBinding('Adonis/Core/Config').get('trpc', {})
+
+    this.app.container.bind('Pyxlab/Adonis/Trpc', () => {
+      const trpc = require('../src/TRPC')
+
+      return trpc
+    })
+
+    this.app.container.bind('Pyxlab/Adonis/TrpcMiddleware', () => {
+      const { TrpcMiddleware } = require('../src/TrpcMiddleware')
+
+      return new TrpcMiddleware(config)
+    })
+  }
+}
