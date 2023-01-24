61fc689d5e6f275971bc6a8c36ebeb1edd473981
Walaff Fernandes <walaffonofre@icloud.com>
2023-01-24 00:31:58 -0300

    chore: :bug: fix type inference issue in @ioc:Adonis/Lucid/Orm module
    The type inference issue in the @ioc:Adonis/Lucid/Orm module has been fixed. This problem was causing unexpected behavior when trying to infer the types of properties within a Lucid ORM model.
    
    The fix involved updating the helper types and recursive functions used in the module to correctly infer the types of properties within a model. Additionally, the ability to exclude properties from the inference process was added by passing the keys of the properties as a second parameter to the InferTypeModel type.
    
    This change ensures type safety and improves the documentation of the model's properties, thus avoiding any confusion or unexpected behavior.

--- adonis-typings/adonis-lucid.ts
+++ adonis-typings/adonis-lucid.ts
@@ -1,7 +1,19 @@
 declare module '@ioc:Adonis/Lucid/Orm' {
+  /**
+   * The `InstanceTypeModel` type is a helper type that is used to infer the instance type of a `LucidModel`
+   */
   type InstanceTypeModel = InstanceType<LucidModel>
 
+  /**
+   * The `KeysOfInstanceTypeModel` type is a helper type that is used to extract the keys of the `InstanceTypeModel`
+   */
   type KeysOfInstanceTypeModel = keyof InstanceTypeModel
+
+  /**
+   * The `KeysOfInstanceTypeRelations` type is a helper type that extends `KeysOfInstanceTypeModel`
+   * and includes additional keys used in the ORM relationships such as 'instance', 'model',
+   * 'client', 'builder', 'subQuery', and '__opaque_type'.
+   */
   type KeysOfInstanceTypeRelations =
     | KeysOfInstanceTypeModel
     | 'instance'
@@ -11,23 +23,51 @@ declare module '@ioc:Adonis/Lucid/Orm' {
     | 'subQuery'
     | '__opaque_type'
 
+  /**
+   * The `Primitive` type is a helper type that defines the basic types that can be inferred
+   */
   type Primitive = string | number | boolean | undefined | null | Date
 
-  type DeepOmitHelper<T, K extends keyof T> = {
+  /**
+   * The `DeepInferTypeModelHelper` is a recursive type that infers the type of the properties within a model.
+   * It uses the `KeysOfInstanceTypeModel` and `Primitive` helper types to infer the type of properties.
+   * `T` is the type of the model, `K` is the keys that are excluded from the inference.
+   */
+  type DeepInferTypeModelHelper<T, K extends keyof T> = {
     [P in K]: T[P] extends infer O
       ? O extends Primitive
         ? O
         : O extends InstanceTypeModel[]
-        ? DeepOmitArray<O, K>
-        : DeepOmit<O, KeysOfInstanceTypeRelations | K>
+        ? DeepInferTypeModelArray<Omit<O, KeysOfInstanceTypeRelations>, K>
+        : DeepInferTypeModel<O, K | KeysOfInstanceTypeRelations>
       : never
   }
 
-  export type DeepOmitArray<T extends InstanceTypeModel[], K = keyof any> = {
-    [P in keyof T]: DeepOmit<T[P], KeysOfInstanceTypeRelations | K>
+  /**
+   * The `DeepInferTypeModelArray` type is a helper type that infers the type of array properties within a model.
+   * `T` is the type of the model, `K` is the keys that are excluded from the inference.
+   */
+  export type DeepInferTypeModelArray<T, K> = {
+    [P in keyof T]: DeepInferTypeModel<T[P], KeysOfInstanceTypeRelations | K>
   }
 
-  type DeepOmit<T, K = keyof any> = T extends Primitive ? T : DeepOmitHelper<T, Exclude<keyof T, K>>
+  /**
+   * The `DeepInferTypeModel` type is a recursive type that infers the type of the properties within a model.
+   * It uses the `KeysOfInstanceTypeModel` and `Primitive` helper types to infer the type of properties.
+   * `T` is the type of the model, `K` is the keys that are excluded from the inference.
+   */
+  type DeepInferTypeModel<T, K> = T extends Primitive
+    ? T
+    : DeepInferTypeModelHelper<T, Exclude<keyof T, K>>
 
-  export type InferTypeModel<Model, K = keyof any> = DeepOmit<Model, KeysOfInstanceTypeModel | K>
+  /**
+   * The `InferTypeModel` type is the final exported type that can be used to infer the types of properties
+   * within a Lucid ORM model. It uses the `DeepInferTypeModel` and `KeysOfInstanceTypeModel` helper types
+   * to infer the types of properties within the model.
+   * `Model` is the type of the model you want to infer the properties from, `K` is the keys that are excluded from the inference.
+   */
+  export type InferTypeModel<Model, K extends keyof Model = never> = DeepInferTypeModel<
+    Model,
+    K | KeysOfInstanceTypeModel
+  >
 }
